560078783a4db9b02889874303297de35cce5a5b
Popcornio <daniel.q.pacheco@gmail.com>
2017-03-31 21:31:41 -0500

    Work on CustomerDaoImpl
    Finished the skeleton for most of the queries. Issues with adding to
    weak entities.

--- src/cs4347/jdbcProject/ecomm/dao/impl/CustomerDaoImpl.java
+++ src/cs4347/jdbcProject/ecomm/dao/impl/CustomerDaoImpl.java
@@ -1,8 +1,12 @@
 package cs4347.jdbcProject.ecomm.dao.impl;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import cs4347.jdbcProject.ecomm.dao.CustomerDAO;
@@ -11,47 +15,254 @@ import cs4347.jdbcProject.ecomm.util.DAOException;
 
 public class CustomerDaoImpl implements CustomerDAO
 {
+	private static final String insertSQL =
+			"INSERT INTO CUSTOMER (firstName, lastName, gender, dob, email, address, creditCard) "
+			+ "VALUES (?, ?, ?, ?, ?, ?, ?);";
+	
+	private static final String selectSQL =
+			"SELECT id, firstName, lastName, gender, dob, email, address, creditCard "
+			+ "FROM customer where id = ?;";
+	
+	private static String updateSQL = 
+			"UPDATE customer SET firstName = ?, lastName = ?, gender = ?, dob = ?, email = ?, address = ?, creditCard = ? "
+			+ "WHERE id = ?;";
+	
+	final static String deleteSQL =
+			"DELETE FROM customer WHERE id = ?;";
+	
+	final static String selectZipcodeSQL = 
+			"SELECT id, firstName, lastName, gender, dob, email, address, creditCard "
+			+ "FROM customer WHERE zipcode = ?;";
+	
+	final static String selectDobSQL = 
+			"SELECT id, firstName, lastName, gender, dob, email, address, creditCard "
+			+ "FROM customer WHERE dob BETWEEN ? AND ?;";
 
-	@Override
-	public Customer create(Connection connection, Customer customer)
-			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+	public Customer create(Connection connection, Customer customer) throws SQLException, DAOException
+	{
+		if(customer.getId() != null)
+		{
+			throw new DAOException("Throw to insert Customer with NON-NULL ID");
+		}
+		
+		PreparedStatement ps = null;
+		try
+		{
+			ps = connection.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, customer.getFirstName());
+			ps.setString(2, customer.getLastName());
+			ps.setString(3, Character.toString(customer.getGender()));
+			//ps.setString(4, customer.getDob());
+			ps.setString(5, customer.getEmail());
+			//ps.setString(6, customer.getAddress());
+			//ps.setString(7, customer.getCreditCard());
+			
+			int res = ps.executeUpdate();
+			if(res != 1)
+			{
+				throw new DAOException("Create Did Not Update Expected Number Of Rows");
+			}
+			
+			// Copies the generated auto-increment primary key to the customer ID
+			ResultSet keyRS = ps.getGeneratedKeys();
+			keyRS.next();
+			int lastKey = keyRS.getInt(1);
+			customer.setId((long) lastKey);
+			
+			return customer;
+		}
+		finally
+		{
+			if(ps != null && !ps.isClosed())
+			{
+				ps.close();
+			}
+		}
 	}
 
-	@Override
-	public Customer retrieve(Connection connection, Long id)
-			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+
+	public Customer retrieve(Connection connection, Long id) throws SQLException, DAOException
+	{
+		if(id == null)
+		{
+			throw new DAOException("Trying to retrieve Customer with NULL ID");
+		}
+		
+		PreparedStatement ps = null;
+		try
+		{
+			ps = connection.prepareStatement(selectSQL);
+			ps.setLong(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(!rs. next())
+			{
+				return null;
+			}
+			
+			Customer cust = new Customer();
+			cust.setId(rs.getLong("id"));
+			cust.setFirstName(rs.getString("firstName"));
+			cust.setLastName(rs.getString("lastName"));
+			cust.setGender(rs.getString("gender").charAt(0));
+			//cust.setDate();
+			cust.setEmail(rs.getString("email"));
+			//cust.setAddress();
+			//cust.setCreditCard();
+			
+			return cust;
+		}
+		finally
+		{
+			if(ps != null & !ps.isClosed())
+			{
+				ps.close();
+			}
+			if(connection != null && !connection.isClosed())
+			{
+				connection.close();
+			}
+		}
 	}
 
-	@Override
-	public int update(Connection connection, Customer customer)
-			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return 0;
+
+	public int update(Connection connection, Customer customer) throws SQLException, DAOException
+	{
+		if (customer.getId() == null)
+		{
+			throw new DAOException("Trying to update Customer with NULL ID");
+		}
+		
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(updateSQL);
+			ps.setString(1, customer.getFirstName());
+			ps.setString(2, customer.getLastName());
+			ps.setString(3, Character.toString(customer.getGender()));
+			//ps.setString(4, customer.getDob());
+			ps.setString(5, customer.getEmail());
+			//ps.setString(6, customer.getAddress());
+			//ps.setString(7, customer.getCreditCard());
+
+			int rows = ps.executeUpdate();
+			return rows;
+		}
+		finally
+		{
+			if (ps != null && !ps.isClosed())
+			{
+				ps.close();
+			}
+			if (connection != null && !connection.isClosed())
+			{
+				connection.close();
+			}
+		}
 	}
 
-	@Override
-	public int delete(Connection connection, Long id) throws SQLException,
-			DAOException {
-		// TODO Auto-generated method stub
-		return 0;
+
+	public int delete(Connection connection, Long id) throws SQLException, DAOException
+	{
+		if (id == null)
+		{
+			throw new DAOException("Trying to delete Customer with NULL ID");
+		}
+		
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(deleteSQL);
+			ps.setLong(1, id);
+
+			int rows = ps.executeUpdate();
+			return rows;
+		}
+		finally
+		{
+			if (ps != null && !ps.isClosed())
+			{
+				ps.close();
+			}
+			if (connection != null && !connection.isClosed())
+			{
+				connection.close();
+			}
+		}
 	}
 
-	@Override
-	public List<Customer> retrieveByZipCode(Connection connection,
-			String zipCode) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+
+	public List<Customer> retrieveByZipCode(Connection connection, String zipCode) throws SQLException, DAOException
+	{
+		PreparedStatement ps = null;
+		try
+		{
+			ps = connection.prepareStatement(selectZipcodeSQL);
+			ps.setString(1, zipCode);
+			ResultSet rs = ps.executeQuery();
+
+			List<Customer> result = new ArrayList<Customer>();
+			while (rs.next())
+			{
+				Customer cust = new Customer();
+				cust.setId(rs.getLong("id"));
+				cust.setFirstName(rs.getString("firstName"));
+				cust.setLastName(rs.getString("lastName"));
+				cust.setGender(rs.getString("gender").charAt(0));
+				//cust.setDate();
+				cust.setEmail(rs.getString("email"));
+				//cust.setAddress();
+				//cust.setCreditCard();
+				result.add(cust);
+			}
+			return result;
+		}
+		finally {
+			if (ps != null && !ps.isClosed())
+			{
+				ps.close();
+			}
+			if (connection != null && !connection.isClosed())
+			{
+				connection.close();
+			}
+		}
 	}
 
-	@Override
-	public List<Customer> retrieveByDOB(Connection connection, Date startDate,
-			Date endDate) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+
+	public List<Customer> retrieveByDOB(Connection connection, Date startDate, Date endDate) throws SQLException, DAOException
+	{
+		PreparedStatement ps = null;
+		try
+		{
+		ps = connection.prepareStatement(selectDobSQL);
+		//ps.setString(1, startDate);
+		//ps.setString(2, endDate);
+		ResultSet rs = ps.executeQuery();
+		
+		List<Customer> result = new ArrayList<Customer>();
+		while (rs.next())
+		{
+			Customer cust = new Customer();
+			cust.setId(rs.getLong("id"));
+			cust.setFirstName(rs.getString("firstName"));
+			cust.setLastName(rs.getString("lastName"));
+			cust.setGender(rs.getString("gender").charAt(0));
+			//cust.setDate();
+			cust.setEmail(rs.getString("email"));
+			//cust.setAddress();
+			//cust.setCreditCard();
+			result.add(cust);
+		}
+		return result;
+		}
+		finally {
+			if (ps != null && !ps.isClosed())
+			{
+				ps.close();
+			}
+			if (connection != null && !connection.isClosed())
+			{
+				connection.close();
+			}
+		}
 	}
 	
 }
