3980fb6d1485396930100ba602ebcefb7a6da20b
ekizoglu <meekizoglu@gmail.com>
2017-04-02 18:00:29 -0500

    Add files via upload

--- dba/create-simple-company-schema.sql
+++ dba/create-simple-company-schema.sql
@@ -18,14 +18,14 @@ USE `simple_company` ;
 -- Table `simple_company`.`Customer`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `simple_company`.`Customer` (
-  `id` BIGINT(20) NOT NULL,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `firstName` VARCHAR(45) NOT NULL,
   `lastName` VARCHAR(45) NOT NULL,
   `gender` CHAR(1) NOT NULL,
   `dob` DATE NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `address` VARCHAR(45) NOT NULL,
-  `creditCard` VARCHAR(45) NOT NULL,
+  `address` VARCHAR(45) NOT NULL DEFAULT 'N/A',
+  `creditCard` VARCHAR(45) NOT NULL DEFAULT 'N/A',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -53,7 +53,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `simple_company`.`Address` (
   `address1` VARCHAR(45) NOT NULL,
-  `address2` VARCHAR(45) NOT NULL,
+  `address2` VARCHAR(45) DEFAULT 'N/A',
   `city` VARCHAR(45) NOT NULL,
   `state` VARCHAR(45) NOT NULL,
   `zipcode` VARCHAR(45) NOT NULL,
@@ -71,7 +71,7 @@ ENGINE = InnoDB;
 -- Table `simple_company`.`Purchase`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `simple_company`.`Purchase` (
-  `id` BIGINT(20) NOT NULL,
+  `id` BIGINT(20) NOT NULL, 
   `productID` BIGINT(20) NOT NULL,
   `customerID` BIGINT(20) NOT NULL,
   `purchaseDate` DATE NOT NULL,
@@ -90,16 +90,16 @@ ENGINE = InnoDB;
 -- Table `simple_company`.`Product`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `simple_company`.`Product` (
-  `id` BIGINT(20) NOT NULL,
+  `id` BIGINT(20) NOT NULL DEFAULT 0,
   `prodName` VARCHAR(45) NOT NULL,
   `prodDescription` VARCHAR(45) NOT NULL,
   `prodCategory` INT NOT NULL,
   `prodUPC` VARCHAR(45) NOT NULL,
-  `Purchase_Customer_id` BIGINT(20) NOT NULL,
+  `Purchase_Customer_id` BIGINT(20) NOT NULL DEFAULT 0,
   PRIMARY KEY (`Purchase_Customer_id`),
   CONSTRAINT `fk_Product_Purchase1`
     FOREIGN KEY (`Purchase_Customer_id`)
-    REFERENCES `simple_company`.`Purchase` (`Customer_id`)
+    REFERENCES `simple_company`.`Customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
